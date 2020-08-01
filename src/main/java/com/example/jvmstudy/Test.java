947306efd6f8bf2c5444d19384b7a76bada08b22
shalongteng <1131790681@qq.com>
2020-08-01 11:02:33 +0800

    init

---
+++ src/main/java/com/example/jvmstudy/Test.java
@@ -0,0 +1,10 @@
+package com.example.jvmstudy;
+
+/**
+ *
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
+}
---
+++ src/test/java/com/example/jvmstudy/JvmStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jvmstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
